8398926be8dc0e6ffcc68116f20e2c89ad91ab01
Anjalijha24 <155735083+Anjalijha24@users.noreply.github.com>
2024-01-05 07:03:55 -0800

    Add files via upload

---
+++ javaprogram.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class javaprogram
+{
+public static void main(String args[])
+{                                                                                                                                                                                                                                     
+String name;
+int age;
+char gender;
+Scanner sc=new Scanner(System.in);                                                                                          ;
+System.out.println("Enter the name=");
+Name=sc.next();                                                                                                                                                                                                                                                
+System.out.println("Enter the age=");
+Age=sc.nextInt();
+System.out.println("Enter the gender=");
+Gender=sc.next().CharAt(0);
+System.out.println("Name:"+name+"\nAge:"+age+"\nGender:"+gender);
+
+}
+} 
