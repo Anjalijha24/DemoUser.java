3d699c5cfa8cb6315c27376743492869a0e1d1d9
Anjalijha24 <155735083+Anjalijha24@users.noreply.github.com>
2024-01-07 10:43:57 -0800

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,26 @@
+public class Arithmetic{
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 4;
+
+        // Addition
+        int sum = num1 + num2;
+        System.out.println("Addition: " + sum);
+
+        // Subtraction
+        int difference = num1 - num2;
+        System.out.println("Subtraction: " + difference);
+
+        // Multiplication
+        int product = num1 * num2;
+        System.out.println("Multiplication: " + product);
+
+        // Division
+        int quotient = num1 / num2;
+        System.out.println("Division: " + quotient);
+
+        // Modulo
+        int remainder = num1 % num2;
+        System.out.println("Modulo: " + remainder);
+    }
+}
