e22cf9f0f9ef38dc63709f4df7940cf88220279e
Anjalijha24 <155735083+Anjalijha24@users.noreply.github.com>
2024-01-07 10:34:30 -0800

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+ class Relational
+{
+public static void main(String[] args) 
+	
+{
+	int a=10,b=5;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a and b");
+      
+        if(a==b)
+        {
+        	System.out.println("The number is true");
+        }
+        else
+        {
+        	System.out.println("The number is false");
+        }
+        
+        if(a!=b)
+        {
+        	System.out.println("The number is true");
+        }
+        else
+        {
+        	System.out.println("The number is false");
+        }
+        if(a>b)
+        {
+        	System.out.println("The number is true");
+        }
+        else
+        {
+        	System.out.println("The number is false");
+        }
+        if(a<b)
+        {
+        	System.out.println("The number is true");
+        }
+        else
+        {
+        	System.out.println("The number is false");
+        }
+        if(a>=b)
+        {
+        	System.out.println("The number is true");
+        }
+        else
+        {
+        	System.out.println("The number is false");
+        }
+        if(a<=b)
+        {
+        	System.out.println("The number is true");
+        }
+        else
+        {
+        	System.out.println("The number is false");
+        }
+	}
+
+}
+
